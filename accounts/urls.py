902a6c75e5211507850dc361ce1b578bb987b527
mohammad noor <mohammadnoormerai@gmail.com>
2025-08-18 16:12:50 +0100

    Registration & Profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
 from accounts.forms import LoginForm
-
+from accounts.views import RegistrationView, edit_profile
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('login/', LoginView.as_view(authentication_form=LoginForm), name='login'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('profile/', edit_profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from accounts.forms import UserRegistrationForm, ProfileForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+class RegistrationView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'registration/register.html'
+    #success_url = reverse_lazy('login')
+
+    def get_success_url(self):
+        login(self.request, self.object)
+        return reverse_lazy('project_list')
+
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user)
+        return render(request, 'profile.html', {'form': form})
